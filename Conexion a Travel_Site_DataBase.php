0d3a02ee564a10a1bf65ef67705ce9b277602074
bj1604 <bradleyburrage@gmail.com>
2025-05-31 17:41:27 +0200

    Corregido Conexion a Travel_Site_DataBase.php

--- Conexion a Travel_Site_DataBase.php
+++ Conexion a Travel_Site_DataBase.php
@@ -1,12 +1,15 @@
 <?php
+//PONER VUESTRO USERNAME Y PASS DE POSTGRES EN "DB_Conection.php" QUE HAY EN LA CARPETA WEB. ES VUESTRO CONFIG INDIVIDUAL, ESTA AÑADIDO AL .gitignore
 $host = "localhost"; // <------ Configurar el local host con xampp
 $port = "5432"; // <------- Puerto de entrada postgres
-$dbname = "Tavel_Site_DB"; // <------- Nombre de la base de datos.
+$dbname = "Tavel_Site_DataBase"; // <------- Nombre de la base de datos.
 $user = "postgres"; // <------ Aqui tu nombre usuario, "postgres" es el normal predeterminado.
 $password = " "; // <------ Aquí tu contraseña de postgres.
 
 $conn = pg_connect("host=$host port=$port dbname=$dbname user=$user password=$password");
 if (!$conn) {
     echo "Error: Unable to connect to the database.\n";
+}else{
+    echo "CONEXIÓN!";
 }
 ?>
